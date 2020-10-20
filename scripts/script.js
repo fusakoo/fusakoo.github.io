d75803ec3ad30a203fafe7b3446196f39b420b76
fusakoo <obatafusako@gmail.com>
2020-10-20 13:31:19 -0700

    Add more content + update CSS

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 // script for navbar toggle button (currently unused)
 
 $(document).ready(function () {
-
+    $("#sidebar").mCustomScrollbar({
+         theme: "minimal"
+    });
     $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
         $('#sidebar').toggleClass('active');
     });
-
 });
